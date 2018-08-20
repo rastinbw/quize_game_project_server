b01bca242a7b1783bd1f58bc5b91a61a42407d60
Alireza <alireza.xcvii@gmail.co git config --global user.name Your>
2018-08-20 21:52:33 +0430

    cache setting and test added

--- source/web/views.py
+++ source/web/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse,JsonResponse
 import json
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.core.cache import cache
+import time
 
 @method_decorator(csrf_exempt, name='dispatch')
 def test(request):
@@ -16,5 +18,18 @@ def test(request):
     # # test = {
     # #     'hello':new_hello
     # # }
+    user = 'ali'
+
+    cache.set( user, {
+        'firstname': 'alireza',
+        'age': 21,
+        'list': [1, 2, 3, 4],
+        'tuple': (1, 2, 3, 4),
+        'dict': {'A': 1, 'B': 2},
+    },timeout = 1)
+
+    print(cache.get(user))
+    time.sleep(2)
+    print(cache.get(user))
 
     return HttpResponse("hello")
